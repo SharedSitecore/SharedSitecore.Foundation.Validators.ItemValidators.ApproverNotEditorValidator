5b8dcf321f43e4fb09275396a6d5e4570060fd0e
David Walker <dave@sitecoredave.com>
2023-03-06 15:35:14 -0600

    init

---
+++ src/ApproverNotEditorValidator.cs
@@ -0,0 +1,27 @@
+﻿using Sitecore.Data.Validators;
+using Sitecore.Data.Items;
+
+namespace SharedSitecore.Foundation.Validators.ItemValidators.ApproverNotEditorValidator {
+    public class ApproverNotEditorValidation : StandardValidator
+    {
+        public override string Name => "Prevent self-approval validator";
+
+        protected override ValidatorResult Evaluate() {
+            Item item = GetItem();
+            if (item != null && item.State != null && item.State.GetWorkflow() != null) {
+                Sitecore.Workflows.IWorkflow workflow = item.State.GetWorkflow();
+                if (workflow.GetState(item) != null && workflow.GetState(item).FinalState) {
+                    if (item.Statistics.UpdatedBy == Sitecore.Context.User.Name) {
+                        Text = "You cannot approve your own content item";
+                        return GetFailedResult(ValidatorResult.Error);
+                    }
+                }
+            }
+            return ValidatorResult.Valid;
+        }
+
+        protected override ValidatorResult GetMaxValidatorResult() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
